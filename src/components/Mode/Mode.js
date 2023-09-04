398e19afac2d4087c9dcd38a977da62aa81d8a4c
Mooka <mooka3084@gmail.com>
2023-09-04 15:12:56 +0100

    portfolio

---
+++ src/components/Mode/Mode.js
@@ -0,0 +1,54 @@
+import "./Mode.css";
+import { RiSunFill, RiMoonFill } from "react-icons/ri";
+import linkedin from "../../assets/linkedin.jpg"
+export default function Mode({ handleModeChange, setMode }) {
+  return (
+    <section className=" container-mode">
+      <div className="mode-left white-text">
+        <ul>
+          <li>
+            <a href="#projects" >Projects</a>
+            
+          </li>
+          <li>
+            <a target="_blank" href="https://github.com/mooka007">
+              <span class="link-decoration">Github</span>
+              <span class="icon-change">
+                <img
+                  src="https://cdn.sanity.io/images/tljndwv6/production/182744b0662c24bb7d37846566bdb7ebee3470d6-15x15.svg"
+                  alt="External Link"
+                />
+                <img
+                  src="https://cdn.sanity.io/images/tljndwv6/production/5fa75a7c756062d52b4f0d9244759279dd216201-15x15.svg"
+                  alt="GitHub"
+                />
+              </span>
+            </a>
+          </li>
+          <li>
+          <a target="_blank" href="https://github.com/mooka007">
+              <span class="link-decoration">LinkedIn</span>
+              <span class="icon-change">
+                <img
+                  src="https://cdn.sanity.io/images/tljndwv6/production/182744b0662c24bb7d37846566bdb7ebee3470d6-15x15.svg"
+                  alt="External Link"
+                />
+                <img
+                  src={linkedin}
+                  alt="GitHub"
+                />
+              </span>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <button onClick={handleModeChange} className="container-icon">
+        {setMode ? (
+          <RiMoonFill className="mode-icon" />
+        ) : (
+          <RiSunFill className="mode-icon" />
+        )}
+      </button>
+    </section>
+  );
+}
